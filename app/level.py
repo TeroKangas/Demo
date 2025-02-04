b04296bdf5655690d2d103a02c039cce73df27cd
Soleya <lucie.haas.06@outlook.de>
2025-02-04 14:42:42 +0100

    added levelup messages, added comments for tables

--- app/level.py
+++ app/level.py
@@ -1,9 +1,12 @@
 import sqlite3
+
+from nicegui import ui
+
 import user
 
 
 class LevelSystem:
-    def __init__(self, db_path, user_id, max_level=100, xp_per_level=50):
+    def __init__(self, db_path, user_id, max_level=100, xp_per_level=15):
         self.conn = sqlite3.connect(db_path)
         self.cursor = self.conn.cursor()
         self.user_id = user_id
@@ -33,40 +36,25 @@ class LevelSystem:
     def add_xp(self, xp):
         result = self.load_user_data()
         self.current_level = result[0]
+        checkLevelValue = result[0]
         self.current_xp = result[1]
         if self.current_level < self.max_level:
             self.current_xp += xp
             print(f"XP hinzugefügt: {xp}. Gesamt XP: {self.current_xp}")
             self.check_level_up()
+            self.save_user_data()
+            if checkLevelValue < self.current_level:
+                ui.notify(f"Congratulations! You are now level {self.current_level}", type='positive')
         else:
             print("Maximales Level erreicht!")
-        self.save_user_data()
+            self.save_user_data()
+            ui.notify(f"Congratulations! You hit the max level of 100!", type='positive')
 
     def check_level_up(self):
         while self.current_xp >= self.xp_per_level and self.current_level < self.max_level:
             self.current_xp -= self.xp_per_level
             self.current_level += 1
             print(f"Level aufgestiegen! Aktuelles Level: {self.current_level}")
-        self.cursor.execute('''
-                    SELECT race 
-                    FROM user 
-                    WHERE is_active = 1;
-                ''')
-        result = self.cursor.fetchone()
-        if result:
-            if result[0] == "Easy":
-                return 2
-
-            elif result[0] == "Normal":
-                return 5
-
-            elif result[0] == "Hard":
-                return 10
-
-            else:
-                return None
-        else:
-            return None
 
     def get_current_state(self):
         return f"Level: {self.current_level}, XP: {self.current_xp}/{self.xp_per_level}"
